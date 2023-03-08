fa674343b2660745580c00ca58354a3192002073
cheppymk <119892722+cheppymk@users.noreply.github.com>
2023-03-08 23:35:56 +0100

    addition to homework

---
+++ CsharpBasic/CSBasic-Homework-03/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = new int[6];
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write("Enter integer no." + (i + 1) + ": ");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+foreach (int number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        sum += number;
+    }
+}
+
+Console.WriteLine("The result is: " + sum);
---
+++ CsharpBasic/CSBasic-Homework-03/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+string[] studentsG2 = { "Ana", "Maja", "Ivan", "Dario", "Jana" };
+
+Console.Write("Enter student group: ");
+if (int.TryParse(Console.ReadLine(), out int group) && group >= 1 && group <= 2)
+{
+    string[] students = group == 1 ? studentsG1 : studentsG2;
+
+    Console.WriteLine("The Students in G" + group + " are:");
+    foreach (string student in students)
+    {
+        Console.WriteLine(student);
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter a number between 1 and 2.");
+}
